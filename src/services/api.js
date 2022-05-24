bf6ee19f33634d68bfd962cb40e00254f24df409
Matheus Schittini <matheusschittinimc@gmail.com>
2022-05-24 15:48:26 -0300

    Implementa a requisiçao a API - Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,25 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const endPoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const result = await fetch(endPoint);
+    const objectResult = await result.json();
+    return objectResult;
+  } catch (error) {
+    return error;
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  let result;
+  try {
+    if (!categoryId) {
+      result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=${query}`);
+    } else {
+      result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=$QUERY`);
+    }
+    const obecjtResult = await result.json();
+    return obecjtResult;
+  } catch (error) {
+    return error;
+  }
 }
